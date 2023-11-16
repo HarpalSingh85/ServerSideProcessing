e0c9397d3a6f4198650859dc2b39291d3e11977c
Harpal Singh <harpal.singh2@hotmail.com>
2023-11-16 17:58:00 +0000

    Upgraded to .Net Framework 4.8

--- ServerSideProcessing/Models/DBModels.Designer.cs
+++ ServerSideProcessing/Models/DBModels.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Youtube Channel\Dotnet Mob\jQuery Datatable in Asp.Net MVC\02 - Server Side Processing\project\ServerSideProcessing\ServerSideProcessing\Models\DBModels.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\harpa\source\repos\ServerSideProcessing\Models\DBModels.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
